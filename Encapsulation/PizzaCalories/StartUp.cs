d9982e2b043665e6c1bc838ba4848d91e19c175d
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-11-10 12:24:50 +0200

    Add files via upload

---
+++ Encapsulation/PizzaCalories/StartUp.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace PizzaCalories
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+                try
+                {
+                    Pizza pizza = CreatePizza();
+                    Dough dough = CreateDough();
+
+                    pizza.Dough = dough;
+                    AddToppingsToPizza(pizza);
+                    Console.WriteLine(pizza.ToString());
+                }
+
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+
+            }
+
+            private static void AddToppingsToPizza(Pizza pizza)
+            {
+                string command;
+                while ((command = Console.ReadLine()) != "END")
+                {
+                    string[] toppingArgs = command
+                        .Split()
+                        .Skip(1)
+                        .ToArray();
+                    string toppingType = toppingArgs[0];
+                    double toppingWeight = double.Parse(toppingArgs[1]);
+                    Topping topping = new Topping(toppingType, toppingWeight);
+                    pizza.AddTopping(topping);
+                }
+            }
+
+            private static Dough CreateDough()
+            {
+                string[] doughArgs = Console.ReadLine()
+                                             .Split()
+                                             .ToArray();
+                string flourtype = doughArgs[1];
+                string bakingTechnique = doughArgs[2];
+                double doughtWeight = double.Parse(doughArgs[3]);
+                Dough dough = new Dough(flourtype, bakingTechnique, doughtWeight);
+                return dough;
+            }
+
+            private static Pizza CreatePizza()
+            {
+                string[] pizzaArgs = Console.ReadLine()
+                                .Split()
+                                .ToArray();
+                string pizzaName = pizzaArgs[1];
+                Pizza pizza = new Pizza(pizzaName);
+                return pizza;
+            }
+        }
+    }
+
