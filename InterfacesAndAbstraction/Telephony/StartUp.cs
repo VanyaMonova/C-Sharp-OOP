ae0718d88fc61ce19fa85eee868972f7c6449f43
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-11-10 12:32:12 +0200

    Add files via upload

---
+++ InterfacesAndAbstraction/Telephony/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace Telephony
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split();
+
+            foreach (string number in numbers)
+            {
+                if (!number.All(char.IsDigit))
+                {
+                    Console.WriteLine("Invalid number!");
+                    continue;
+                }
+
+                ICallable phone;
+
+                if (number.Length == 10)
+                {
+                    phone = new Smartphone();
+                }
+                else
+                {
+                    phone = new StationaryPhone();
+                }
+
+                phone.Call(number);
+            }
+
+            string[] urls = Console.ReadLine().Split();
+
+            foreach (string url in urls)
+            {
+                if (url.Any(char.IsDigit))
+                {
+                    Console.WriteLine("Invalid URL!");
+                    continue;
+                }
+
+                IBrowsable smartphone = new Smartphone();
+                smartphone.Browse(url);
+            }
+        }
+    }
+}
