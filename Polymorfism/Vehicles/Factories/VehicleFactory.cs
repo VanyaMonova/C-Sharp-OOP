fbe80838051039610c712510cf435faa84cea2d5
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-11-14 23:23:32 +0200

    Add files via upload

---
+++ Polymorfism/Vehicles/Factories/VehicleFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vehicles.Models;
+using Vehicles.Models.Common;
+
+namespace Vehicles.Factories
+{
+    public class VehicleFactory
+    {
+        public VehicleFactory()
+        {
+
+        }
+
+        public Vehicle CreateVehicle(string vehicleType, double fuelQuantity, double fuelConsumption)
+        {
+            Vehicle vehicle;
+
+            if (vehicleType == "Car")
+            {
+                vehicle = new Car(fuelQuantity, fuelConsumption);
+            }
+            else if (vehicleType == "Truck")
+            {
+                vehicle = new Truck(fuelQuantity, fuelConsumption);
+            }
+            else
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidVehicleType);
+            }
+
+            return vehicle;
+        }
+    }
+}
