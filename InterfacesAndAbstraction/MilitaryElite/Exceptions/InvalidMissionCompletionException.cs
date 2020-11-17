6d505b63f30aed4aa806a8c6348f0ae88a54813a
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-11-18 01:09:03 +0200

    Add files via upload

---
+++ InterfacesAndAbstraction/MilitaryElite/Exceptions/InvalidMissionCompletionException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Exceptions
+{
+    public class InvalidMissionCompletionException : Exception
+    {
+        private const string DEF_EXC_MSG = "Mission already completed!";
+
+        public InvalidMissionCompletionException() : base(DEF_EXC_MSG)
+        {
+        }
+
+        public InvalidMissionCompletionException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ InterfacesAndAbstraction/MilitaryElite/Models/Private.cs
@@ -0,0 +1,25 @@
+﻿using MilitaryElite.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models
+{
+    public class Private : Soldier, IPrivate
+    {
+        public Private(int id, string firstName, string lastName, decimal salary) 
+            : base(id, firstName, lastName)
+        {
+            this.Salary = salary;
+        }
+
+        public decimal Salary { get; private set; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(base.ToString());
+            sb.AppendLine($" Salary: {this.Salary:F2}");
+            return sb.ToString().Trim();
+        }
+    }
+}
